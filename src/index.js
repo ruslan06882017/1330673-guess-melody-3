816d572b8293f862976d22f80dfd79f800b84e4d
Ruslan G <r.gusseinov@mail.ru>
2020-02-23 16:57:41 +0600

    Added artist and genre components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/app/app.jsx';
+import questions from './mocks/questions.js';
 
 const Settings = {
   ERROR_COUNT: 3
@@ -13,6 +14,7 @@ ReactDOM.render(
     <App
       errorsCount = {Settings.ERROR_COUNT}
       onWelcomeButtonClick = {onWelcomeButtonClick}
+      questions = {questions}
     />,
     document.querySelector(`#root`)
 );
